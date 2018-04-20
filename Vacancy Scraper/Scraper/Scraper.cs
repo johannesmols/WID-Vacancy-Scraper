0a37a3098579c72763953653766389080b11694b
Johannes Mols <johannes.mols@googlemail.com>
2018-04-20 22:56:54 +0200

    Added Selenium Packages

--- Vacancy Scraper/Scraper/Scraper.cs
+++ Vacancy Scraper/Scraper/Scraper.cs
@@ -3,12 +3,15 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+using Vacancy_Scraper.JsonManagers;
 using Vacancy_Scraper.Objects;
 
 namespace Vacancy_Scraper.Scraper
 {
-    class Scraper
+    public class Scraper
     {
+        private SettingsManager _settingsManager = new SettingsManager();
+
         public async Task<string> Scrape(CompanyObject company)
         {
             await Task.Delay(2000);
